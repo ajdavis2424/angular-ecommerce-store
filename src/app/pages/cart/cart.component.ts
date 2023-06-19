c175c33914ab077b4cf4f5c89c6514d949db44b3
ALexander Davis <ajdavis2424@gmail.com>
2023-06-19 15:51:33 -0400

    more cart logic

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './cart.component.html'
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  // dummy data
+  cart:Cart = {items:[{
+    product: 'https://via.placeholder.com/150',
+  name: 'snickers',
+  price: 200,
+  quantity: 1,
+  id: 1,
+  }]}
+  dataSource:Array<CartItem> = []
+  displayedColumns:Array<string> = [
+    'product', 'name', 'price', 'quantity', 'total','action'
+  ]
+  
+  constructor() {}
 
   ngOnInit(): void {
+    this.dataSource = this.cart.items
   }
-
 }
