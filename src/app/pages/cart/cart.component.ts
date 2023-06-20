9a32569c9306b2a4d88dcbf79c2c360087159ffa
ALexander Davis <ajdavis2424@gmail.com>
2023-06-20 00:01:51 -0400

    finished cart page and cart logic

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,27 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
-  templateUrl: './cart.component.html'
+  templateUrl: './cart.component.html',
 })
 export class CartComponent implements OnInit {
   // dummy data
-  cart:Cart = {items:[{
-    product: 'https://via.placeholder.com/150',
-  name: 'snickers',
-  price: 200,
-  quantity: 1,
-  id: 1,
-  }]}
-  dataSource:Array<CartItem> = []
-  displayedColumns:Array<string> = [
-    'product', 'name', 'price', 'quantity', 'total','action'
-  ]
-  
-  constructor() {}
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 200,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 200,
+        quantity: 1,
+        id: 2,
+      },
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 200,
+        quantity: 3,
+        id: 1,
+      },
+    ],
+  };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
+
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.dataSource = this.cart.items
+    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cartItem: Cart) => {
+      this.cart = _cartItem;
+      this.dataSource = this.cart.items;
+      // eventually add saving to local storage!
+    });
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item)
   }
 }
--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,14 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
-  templateUrl: './product-box.component.html'
+  templateUrl: './product-box.component.html',
 })
 export class ProductBoxComponent implements OnInit {
-@Input() fullWidthMode = false;
-  constructor() { }
+  @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: 'Snickers',
+    price: 200,
+    category: 'shoes',
+    description: 'Description',
+    image: 'https//via.placeholder.com/150',
+  };
 
-  ngOnInit(): void {
+  @Output() addToCart = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
   }
-
 }
