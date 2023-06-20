dc678181ea211b4a99e33a2576f9dee49d69bd57
ALexander Davis <ajdavis2424@gmail.com>
2023-06-20 09:41:24 -0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,17 @@ import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
-  template: ` <app-header [cart]="cart"></app-header> 
-  <router-outlet></router-outlet>`,
+  template: ` <app-header [cart]="cart"></app-header>
+    <router-outlet></router-outlet>`,
   styles: [],
 })
 export class AppComponent implements OnInit {
-  cart: Cart = {items:[]};
-constructor(private cartService: CartService){}
+  cart: Cart = { items: [] };
+  constructor(private cartService: CartService) {}
 
   ngOnInit() {
-    this.cartService.cart.subscribe((_cart) =>{
-      this.cart =_cart
-    })
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart;
+    });
   }
 }
